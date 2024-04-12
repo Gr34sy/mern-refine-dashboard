b03536e07c7bcaade08eeeab7396458811700eb1
Gr34sy <slupek3@poczta.fm>
2024-04-12 10:05:39 +0200

    successfully imported layout from mui and added index.html, index.css to the root folder and constants folder to src

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,1 +1,6 @@
-export { Header } from "./header";
+
+export * from "./layout/header";
+export * from "./layout/hamburgerMenu";
+export * from "./layout/sider";
+export * from "./layout/index";
+export * from "./layout/title";
