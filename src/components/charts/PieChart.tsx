5d2ba63d3cf160b8095ef9bb53af0f16a83b350d
Gr34sy <slupek3@poczta.fm>
2024-04-30 14:20:30 +0200

    ready piecharts

--- src/components/charts/PieChart.tsx
+++ src/components/charts/PieChart.tsx
@@ -1,32 +1,53 @@
-import React from 'react';
-import { Box, Typography, Stack } from '@mui/material';
+import React from "react";
+import { Box, Typography, Stack } from "@mui/material";
 
 import { PieChartProps } from "../../interfaces/home";
-import ReactApexChart from 'react-apexcharts';
+import ReactApexChart from "react-apexcharts";
 
-const PieChart = ({title, value, series, colors}: PieChartProps) => {
+const PieChart = ({ title, value, series, colors }: PieChartProps) => {
   return (
-    <Box id="chart" flex={1} display="flex" bgcolor="primary.main">
+    <Box
+      id="chart"
+      flex={1}
+      display="flex"
+      bgcolor="primary.main"
+      flexDirection="row"
+      justifyContent="space-between"
+      alignItems="center"
+      pl={3.5}
+      py={2}
+      gap={2}
+      borderRadius="15px"
+      minHeight="110px"
+      width="fit-content"
+    >
       <Stack direction="column">
         <Typography fontSize={14} color="secondary.contrastText">
           {title}
         </Typography>
-        <Typography fontSize={24} color="primary.contrastText" fontWeight={700} mt={1}>
+        <Typography
+          fontSize={24}
+          color="primary.contrastText"
+          fontWeight={700}
+          mt={1}
+        >
           {value}
         </Typography>
       </Stack>
 
-      <ReactApexChart options={{
-          chart: {type: 'donut'},
+      <ReactApexChart
+        options={{
+          chart: { type: "donut" },
           colors,
-          legend: {show: false},
-          dataLabels: {enabled: false},
-      }}
-      type="donut"
-      series={series}
-      width="120px"/>
+          legend: { show: false },
+          dataLabels: { enabled: false },
+        }}
+        type="donut"
+        series={series}
+        width="120px"
+      />
     </Box>
-  )
-}
+  );
+};
 
-export default PieChart
+export default PieChart;
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -17,10 +17,10 @@ const home = () => {
       </Typography>
 
       <Box mt="20px" display="flex" flexWrap="wrap" gap={4}>
-          <PieChart title="Properties for Sale" value={682} series={[75, 12]} colors={['#475be8','#e4e8ef']}/>
-          <PieChart title="Properties for Rent" value={490} series={[60, 40]} colors={['#475ae8','#e4b8ef']}/>
-          <PieChart title="Total customers" value={7292} series={[75, 12]} colors={['#275be8','#c4e8ef']}/>
-          <PieChart title="Properties for Cities" value={375} series={[75, 12]} colors={['#475be8','#e4e8ef']}/>
+          <PieChart title="Properties for Sale" value={682} series={[75, 12]} colors={['#E257A9','#F7BFE0']}/>
+          <PieChart title="Properties for Rent" value={490} series={[60, 40]} colors={['#7841E1','#CAB6EF']}/>
+          <PieChart title="Properties for Cities" value={375} series={[75, 12]} colors={['#25D072','#AFE7C8']}/>
+          <PieChart title="Total Customers" value={7292} series={[75, 12]} colors={['#14B8EC','#AFE0DF']}/>
       </Box>
     </Box>
   );
