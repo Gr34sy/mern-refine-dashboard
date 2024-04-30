7aa6f924b5f64c9f940544e0eee195292bb57832
Gr34sy <slupek3@poczta.fm>
2024-04-30 16:02:20 +0200

    finished total revenue chart

--- src/components/charts/TotalRevenue.tsx
+++ src/components/charts/TotalRevenue.tsx
@@ -1,10 +1,41 @@
-import React from 'react'
+import { Box, Typography, Stack } from "@mui/material";
+import ReactApexChart from "react-apexcharts";
+import { ArrowCircleUpRounded } from "@mui/icons-material";
+
+import { TotalRevenueOptions, TotalRevenueSeries } from "./chart.config";
 
 const TotalRevenue = () => {
   return (
-    <div>
+    <Box p={4} flex={1} bgcolor="primary.main" id="chart" display="flex" flexDirection="column" borderRadius="15px">
       
-    </div>
+
+      <Typography fontSize={18} fontWeight={600} color="primary.contrastText">
+        Total Revenue
+      </Typography>
+
+      <Stack my="20px" direction="row" gap={4} flexWrap="wrap">
+        <Typography fontSize={28} fontWeight={700} color="primary.contrastText">
+          $236,354
+        </Typography>
+
+        <Stack>
+          <ArrowCircleUpRounded sx={{
+            fontSize: 25, color: 'secondary.main'
+          }} />
+
+          <Stack direction="row" alignItems="center" gap={1}>
+            <Typography fontSize={15} color="secondary.main" fontWeight={600}>
+              0.8%
+            </Typography>
+            <Typography fontSize={15} color="secondary.contrastText">
+             Than last Month
+            </Typography>
+          </Stack>
+        </Stack>
+      </Stack>
+
+      <ReactApexChart series={TotalRevenueSeries} type="bar" height={310} options={TotalRevenueOptions}/>
+    </Box>
   )
 }
 
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,5 +1,5 @@
 import { useList } from "@refinedev/core";
-import { Box, Typography} from '@mui/material';
+import { Box, Typography, Stack} from '@mui/material';
 
 import {
   PieChart,
@@ -17,11 +17,16 @@ const home = () => {
       </Typography>
 
       <Box mt="20px" display="flex" flexWrap="wrap" gap={4}>
-          <PieChart title="Properties for Sale" value={682} series={[75, 12]} colors={['#E257A9','#F7BFE0']}/>
+          <PieChart title="Properties for Sale" value={682} series={[75, 12]} colors={['#EF850F','#F5C085']}/>
           <PieChart title="Properties for Rent" value={490} series={[60, 40]} colors={['#7841E1','#CAB6EF']}/>
           <PieChart title="Properties for Cities" value={375} series={[75, 12]} colors={['#25D072','#AFE7C8']}/>
           <PieChart title="Total Customers" value={7292} series={[75, 12]} colors={['#14B8EC','#AFE0DF']}/>
       </Box>
+
+      <Stack mt="25px" width="100%" direction={{xs: 'column', lg: 'row'}}>
+        <TotalRevenue />
+        <PropertyReferrals />
+      </Stack>
     </Box>
   );
 };
