e3954b86af0cd42fbb2e1ff004e354020cb12042
Gr34sy <slupek3@poczta.fm>
2024-05-01 12:30:57 +0200

    started making the form

--- src/components/common/Form.tsx
+++ src/components/common/Form.tsx
@@ -1,10 +1,27 @@
-import React from 'react'
+import { Box, Stack, Typography, FormControl, Button, FormHelperText, TextField, TextareaAutosize, Select, MenuItem } from '@mui/material';
+import { FormProps } from '../../interfaces/common';
+import CustomButton from './CustomButton';
 
-const Form = () => {
+const Form = ({type,register,handleSubmit, handleImageChange, formLoading, onFinishHandler, propertyImage} : FormProps) => {
   return (
-    <div>
-      
-    </div>
+    <Box>
+      <Typography fontSize={25} fontWeight={700} color="primary.contrastText">
+        {type} a Property
+      </Typography>
+
+      <Box mt={2.5} borderRadius="15px" padding="20px" bgcolor="primary.main">
+          <form style={{marginTop: '20px', width: '100%', display: 'flex', flexDirection: 'column', gap: '20px'}} onSubmit={handleSubmit(onFinishHandler)}>
+            <FormControl>
+              <FormHelperText>
+                Enter Property Name
+              </FormHelperText>
+              <TextField>
+
+              </TextField>
+            </FormControl>
+          </form>
+      </Box>
+    </Box>
   )
 }
 
--- src/pages/create-property.tsx
+++ src/pages/create-property.tsx
@@ -1,11 +1,38 @@
-import React from 'react'
+import { useState } from "react";
+import { useGetIdentity } from "@refinedev/core";
+import { useNavigate } from "react-router-dom";
+import { useForm } from "@refinedev/react-hook-form";
+import { FieldValues } from "react-hook-form";
+import Form from "../components/common/Form";
 
-const createProperty = () => {
+const CreateProperty = () => {
+  const navigate = useNavigate();
+  const { data: user } = useGetIdentity();
+  const [propertyImage, setPropertyImage] = useState({
+    name: "",
+    url: "",
+  });
+  const {
+    refineCore: { onFinish, formLoading },
+    register,
+    handleSubmit,
+  } = useForm();
+
+  const onFinishHandler = () => {};
+  const handleImageChange = () => {};
+
   return (
-    <div>
-      create-property
-    </div>
-  )
-}
+    <Form
+      type="Create"
+      register={register}
+      onFinish={onFinish}
+      formLoading={formLoading}
+      handleSubmit={handleSubmit}
+      propertyImage={propertyImage}
+      onFinishHandler={onFinishHandler}
+      handleImageChange={handleImageChange}
+    ></Form>
+  );
+};
 
-export default createProperty
+export default CreateProperty;
