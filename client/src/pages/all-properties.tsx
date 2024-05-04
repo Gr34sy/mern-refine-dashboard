bee3d276a5bf36cef52891605c5aa8ee0f36ddc1
Gr34sy <slupek3@poczta.fm>
2024-05-04 12:31:24 +0200

    finished work on all properties site, added pagination, sorting and filtering

--- client/src/pages/all-properties.tsx
+++ client/src/pages/all-properties.tsx
@@ -9,7 +9,7 @@ import {
   Select,
   MenuItem,
 } from "@mui/material";
-
+import { useMemo } from "react";
 import { PropertyCard, CustomButton } from "../components";
 
 const AllProperties = () => {
@@ -43,6 +43,15 @@ const AllProperties = () => {
   const toggleSort = (field: string) => {
     setSorters([{ field, order: currentPrice === "asc" ? "desc" : "asc" }]);
   };
+
+  const currentFilterValues = useMemo(() => {
+      const logicalFilters = filters.flatMap((item) => ('field' in item ? item : []));
+
+      return {
+        title: logicalFilters.find((item) => item.field === 'title')?.value || '',
+        propertyType: logicalFilters.find((item) => item.field === 'propertyType')?.value || ''
+      }
+  }, [filters])
 
   if (isLoading) return <Typography>Loading...</Typography>;
   if (isError) return <Typography>Error</Typography>;
@@ -83,8 +92,14 @@ const AllProperties = () => {
                   variant="outlined"
                   color="info"
                   placeholder="Search by title"
-                  value=""
-                  onChange={() => {}}
+                  value={currentFilterValues.title}
+                  onChange={(e) => {
+                    setFilters([{
+                      field: 'title',
+                      operator: 'contains',
+                      value: e.currentTarget.value ? e.currentTarget.value : undefined,
+                    }]);
+                  }}
                 />
                 <Select
                   variant="outlined"
@@ -93,10 +108,19 @@ const AllProperties = () => {
                   required
                   inputProps={{ "aria-label": "Without label" }}
                   defaultValue=""
-                  value=""
-                  onChange={() => {}}
+                  value={currentFilterValues.propertyType}
+                  onChange={(e) => {
+                    setFilters([{
+                      field: 'propertyType',
+                      operator: 'eq',
+                      value: e.target.value,
+                    }]);
+                  }}
                 >
                   <MenuItem value="">All</MenuItem>
+                  {['Apartment', 'Villa', 'Farmhouse', 'Condos', 'Townhouse', 'Duplex', 'Studio', 'Chalet'].map((type) =>(
+                    <MenuItem key={type} value={type.toLowerCase()}>{type}</MenuItem>
+                  ))}
                 </Select>
               </Box>
             </Box>
@@ -158,7 +182,7 @@ const AllProperties = () => {
             required
             inputProps={{ "aria-label": "Without label" }}
             defaultValue={10}
-            onChange={() => {}}
+            onChange={(e) => setPageSize(e.target.value ? Number(e.target.value) : 10)}
           >
             {[10, 20, 30, 40, 50].map((size) => (
               <MenuItem key={size} value={size}>
