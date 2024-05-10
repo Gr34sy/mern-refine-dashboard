c4915e691929d60746ec01dbfb19ab694cad4863
Gr34sy <slupek3@poczta.fm>
2024-05-10 11:15:46 +0200

    added client side form code

--- client/src/pages/agent-profile.tsx
+++ client/src/pages/agent-profile.tsx
@@ -1,10 +1,6 @@
-import { useGetIdentity, useOne } from "@refinedev/core";
+import { useOne } from "@refinedev/core";
 import { useParams } from "react-router-dom";
 import { Profile } from "../components";
-import { Box, Typography, Stack } from "@mui/material";
-import { EditLocationAlt } from "@mui/icons-material";
-import { CustomButton } from "../components";
-import { useNavigate } from "react-router-dom";
 import LoadingScreen from "../components/common/LoadingScreen";
 
 const agentProfile = () => {
@@ -14,7 +10,6 @@ const agentProfile = () => {
     id: id as string,
   });
 
-  const navigate = useNavigate();
 
   const myProfile = data?.data ?? [];
 
--- client/src/pages/edit-profile.tsx
+++ client/src/pages/edit-profile.tsx
@@ -1,11 +1,162 @@
 import { useParams } from "react-router-dom";
+import { useState } from "react";
+import {
+  Box,
+  Stack,
+  Typography,
+  FormControl,
+  Button,
+  FormHelperText,
+  TextField,
+} from "@mui/material";
+import { CustomButton } from "../components";
+import { useForm } from "@refinedev/react-hook-form";
+import { FieldValues } from "react-hook-form";
+import { useGetIdentity } from "@refinedev/core";
 
 const editProfile = () => {
-    const { id } = useParams();
+  const [profileImage, setProfileImage] = useState({
+    name: "",
+    url: "",
+  });
+  const { data: user } = useGetIdentity();
+  const { id } = useParams();
+
+  const {
+    refineCore: { onFinish, formLoading },
+    register,
+    handleSubmit,
+  } = useForm();
+
+  const handleImageChange = (file: File) => {
+    const reader = (readFile: File) => new Promise<string>((resolve, reject) => {
+      const fileReader = new FileReader();
+      fileReader.onload = () => resolve(fileReader.result as string);
+      fileReader.readAsDataURL(readFile);
+    });
+
+    reader(file).then((result: string) => setProfileImage({ name: file?.name, url: result }));
+  };
+
+  const onFinishHandler = async (data: FieldValues) => {
+    if(!profileImage.name) return alert('Please select an image');
+
+    if (
+      typeof user === "object" &&
+      user &&
+      "email" in user &&
+      typeof user.email === "string"
+    ){
+      await onFinish({
+        ...data,
+        photo: profileImage.url, 
+        email: user.email,      
+      })
+    }
+  };
+
   return (
-    <div>
+    
+    <Box>
+      <Typography fontSize={25} fontWeight={700} color="primary.contrastText">
       Edit your profile - {id}
-    </div>
+      </Typography>
+
+      <Box mt={2.5} borderRadius="15px" padding="20px" bgcolor="primary.main">
+        <form
+          style={{
+            marginTop: "20px",
+            width: "100%",
+            display: "flex",
+            flexDirection: "column",
+            gap: "20px",
+          }}
+          onSubmit={handleSubmit(onFinishHandler)}
+        >
+          <FormControl>
+            <FormHelperText
+              sx={{
+                fontWeight: 500,
+                margin: "10px 0",
+                fontSize: 16,
+                color: "primary.contrastText",
+              }}
+            >
+              Enter Location
+            </FormHelperText>
+            <TextField
+              fullWidth
+              required
+              id="outlined-basic"
+              color="info"
+              variant="outlined"
+              {...register("user-location", { required: true })}
+            />
+          </FormControl>
+
+          <FormControl>
+            <FormHelperText
+              sx={{
+                fontWeight: 500,
+                margin: "10px 0",
+                fontSize: 16,
+                color: "primary.contrastText",
+              }}
+            >
+              Enter Phone Number
+            </FormHelperText>
+            <TextField
+              fullWidth
+              required
+              id="outlined-basic"
+              color="info"
+              variant="outlined"
+              {...register("phone", { required: true })}
+            />
+          </FormControl>
+
+          <Stack direction="column" gap={1} justifyContent="center" mb={2}>
+            <Stack direction="row" gap={2}>
+              <Typography
+                color="primary.contrastText"
+                fontSize={16}
+                fontWeight={500}
+                my="10px"
+              >
+                Background Image
+              </Typography>
+
+              <Button
+                component="label"
+                sx={{
+                  width: "fit-content",
+                  color: "#2ed480",
+                  textTransform: "capitalize",
+                  fontSize: 16,
+                }}
+              >
+                Upload *
+                <input
+                  hidden
+                  accept="image/*"
+                  type="file"
+                  onChange={(e) => {
+                    // @ts-ignore
+                    handleImageChange(e.target.files[0]);
+                  }}
+                />
+              </Button>
+            </Stack>
+
+            <Typography fontSize={14} color="secondary.contrastText" sx={{wordBreak: 'break-all'}}>
+              {profileImage?.name}
+            </Typography>
+          </Stack>
+
+          <CustomButton type="submit" title={formLoading ? 'Submitting...' : 'Submit'} backgroundColor="secondary.main" color="#fcfcfc" />
+        </form>
+      </Box>
+    </Box>
   )
 }
 
