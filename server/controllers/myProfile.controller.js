775dbc635bf59c5873630dc3cb0e00b31481d623
Gr34sy <piotr.slupski17@gmail.com>
2024-06-20 19:58:26 +0200

    updating

--- server/controllers/myProfile.controller.js
+++ server/controllers/myProfile.controller.js
@@ -13,49 +13,50 @@ cloudinary.config({
 });
 
 const getProfile = async (req, res) => {
-    try{
-        const { id } = req.params;
-        const user = await User.findOne({_id: id});
-     
-        if(user){ res.status(200).json(user)}else{
-           res.status(404).json({message: 'User not found'});
-        };
-       }catch(e){
-        console.log(e.message);
-        res.status(500).json({message: 'Server error'});
-       }
-}
+  try {
+    const { id } = req.params;
+    const user = await User.findOne({ _id: id });
+
+    if (user) {
+      res.status(200).json(user);
+    } else {
+      res.status(404).json({ message: "User not found" });
+    }
+  } catch (e) {
+    console.log(e.message);
+    res.status(500).json({ message: "Server error" });
+  }
+};
 
 const updateProfile = async (req, res) => {
-    try {
-        const { id } = req.params;
-        const {image, phone, location, email } = req.body;
+  try {
+    const { id } = req.params;
+    const { image, phone, location, email } = req.body;
 
-        const emailUser = await User.findOne({email: email});
-        const idUser = await User.findOne({_id: id});
+    const emailUser = await User.findOne({ email: email });
+    const idUser = await User.findOne({ _id: id });
 
-        if(emailUser._id.toString() !== idUser._id.toString()) throw new Error('User id and email do not match');
-    
-        const imageUrl = await cloudinary.uploader.upload(image);
-    
-        await User.findByIdAndUpdate({_id: id}, { 
-            $set: { 
-              location,
-              phone,
-              image: imageUrl.url,
-            }
-          } 
-        )
-    
-        res.status(200).json({message: 'Your profile updated successfully'})
-    
-      } catch (e) {
-        console.log(e.message);
-        res.status(500).json({ message: "Profile update failed" });
+    if (emailUser._id.toString() !== idUser._id.toString())
+      throw new Error("User id and email do not match");
+
+    const imageUrl = await cloudinary.uploader.upload(image);
+
+    await User.findByIdAndUpdate(
+      { _id: id },
+      {
+        $set: {
+          location,
+          phone,
+          image: imageUrl.url,
+        },
       }
-}
+    );
 
-export {
-    getProfile,
-    updateProfile,
-}
+    res.status(200).json({ message: "Your profile updated successfully" });
+  } catch (e) {
+    console.log(e.message);
+    res.status(500).json({ message: "Profile update failed" });
+  }
+};
+
+export { getProfile, updateProfile };
